c3ca723016ca949982328be1d5ef647cb9b1c7e6
jeraldvj <49885786+jeraldvj@users.noreply.github.com>
2021-06-03 11:57:17 -0400

    Add files via upload
    new menú hamburger

---
+++ hamburger.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    document.getElementsByClassName("button_container")[0].classList.toggle("active");
+    document.getElementsByClassName("overlay")[0].classList.toggle("open");
+}
